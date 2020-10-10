9e11af5dc0704d63e430d6564b7a065dfc7af53d
Your Name <1834408905@qq.com>
2020-10-10 12:59:56 +0800

    目录

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import jsonp from 'jsonp'
+
+// import jsonp from 'jsonp'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   mounted(){
-    //通过CORS跨域
-    this.$axios.get('https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list').then({
+    // //通过CORS跨域
+    // this.$axios.get('https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list').then({
 
-    })
-    //通过jsonp跨域
-    jsonp('https://www.imooc.com/common/adver-getadver',(err,res)=>{
-      console.log(res)
-    })
-    //通过接口代理跨域
-    jsonp('/api/activity/servicetime',(err,res)=>{
-      console.log(res)
-    })
+    // })
+    // //通过jsonp跨域
+    // jsonp('https://www.imooc.com/common/adver-getadver',(err,res)=>{
+    //   console.log(res)
+    // })
+    // //通过接口代理跨域
+    // jsonp('/api/activity/servicetime',(err,res)=>{
+    //   console.log(res)
+    // })
   }
 }
 </script>
