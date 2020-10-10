021e7a3a2d53b08590bf3cedbb1f04e341ba5ee3
Your Name <1834408905@qq.com>
2020-10-10 09:09:22 +0800

    项目创建

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+
+Vue.use(VueRouter)
+Vue.use(Vuex)
 
 new Vue({
   render: h => h(App),
