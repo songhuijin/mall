10cfc6c4635e340e2da559134ec8b9f77be8a19a
Your Name <1834408905@qq.com>
2020-10-25 14:44:57 +0800

    参数组件

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    product
+    <ProductParam>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </ProductParam>
+    
   </div>
 </template>
 <script>
-
+import ProductParam from '../components/ProductParam'
 export default {
   name:'product',
   components:{
-    
+    ProductParam
   }
 }
 </script>
