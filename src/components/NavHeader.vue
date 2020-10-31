0dcdfb96e1817a33dace2a14e6ee361f62d0ec92
Your Name <1834408905@qq.com>
2020-10-31 16:31:53 +0800

    退出

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -11,10 +11,12 @@
         <div class="topbar-user">
           <a href="javascript:;" v-if="username">{{username}}</a>
           <a href="javascript:;" v-if="!username" @click="login">登录</a>
+          <a href="javascript:;" v-if="username" @click="logout">退出</a>
           <a href="javascript:;" v-if="username">我的订单</a>
           <a href="javascript:;" class="my-cart" @click="goToCart">
             <span class="icon-cart"></span>购物车{{cartCount}}
           </a>
+          <!-- <a href="javascript:;" v-if="username" @click="logout">退出</a> -->
         </div>
       </div>
     </div>
@@ -143,10 +145,29 @@ export default {
   },
   mounted(){
     this.getProductList();
+    let params = this.$route.params
+    if(params && params.from == 'login'){
+       this.getCartCount();
+    }
+   
+
   },
   methods:{
+    getCartCount(){
+       this.$axios.get('/carts/products/sum').then((res=0)=>{
+         this.$store.dispatch('saveCartCount',res)
+      })
+    },
     login(){
       this.$router.push('/login')
+    },
+    logout(){
+      this.$axios.post('/user/logout').then(()=>{
+        this.$Message.success('退出成功');
+        this.$cookie.set('userId','',{expires:'-1'})
+        this.$store.dispatch('saveUserName','')
+        this.$store.dispatch('saveCartCount',0)
+      })
     },
     getProductList(){
       this.$axios.get('/products',{
