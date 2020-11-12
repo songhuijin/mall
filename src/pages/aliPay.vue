f6e36dffe649bc37d9673f095b4e35dee6b028a1
Your Name <1834408905@qq.com>
2020-11-12 15:00:59 +0800

    支付宝支付

--- src/pages/aliPay.vue
+++ src/pages/aliPay.vue
@@ -1,14 +1,40 @@
 <template>
-  <div>
-    aliPay
+  <div class="ali-pay">
+    <loading v-if="loading"></loading>
+    <div class="form" v-html="content"></div>
   </div>
 </template>
 <script>
-
+import loading from './../components/Loading'
 export default {
   name:'aliPay',
   components:{
-    
+    loading
+  },
+  data(){
+    return{
+      orderId:this.$route.query.orderId,
+      content:'',
+      loading:true,
+    }
+  },
+  mounted(){
+    this.paySubmit()
+  },
+  methods:{
+    paySubmit(){
+      this.$axios.post('/pay',{
+        orderId:this.orderId,
+        orderName:'Vue高仿小米商城',
+        amount:0.01,
+        payType:1
+      }).then(res=>{
+        this.content = res.content;
+        setTimeout(()=>{
+          document.forms[0].submit();
+        },100)
+      })
+    }
   }
 }
 </script>
